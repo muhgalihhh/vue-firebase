a3284823d3e0c421b29d286743fc6ca819fa2999
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-11-20 23:02:21 +0700

    udahan capek

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCjueg7LfljKg9Zrvr0b4hN7KtgUKvPp80',
-  authDomain: 'vue-firebase-f6f1a.firebaseapp.com',
-  projectId: 'vue-firebase-f6f1a',
-  storageBucket: 'vue-firebase-f6f1a.firebasestorage.app',
-  messagingSenderId: '490957249666',
-  appId: '1:490957249666:web:be5929de3752d7f9ee289b',
-  measurementId: 'G-T8K9N2527R',
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebase = initializeApp(firebaseConfig);
